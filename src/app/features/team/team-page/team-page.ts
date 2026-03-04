2b49ba2b3dc14cb0fa12a238b7c78bff285ce17c
Abdelmomen Nasreldin <abdelmomennasreldin@gmail.com>
2026-03-04 14:15:49 +0200

    team page added

--- src/app/features/team/team-page/team-page.ts
+++ src/app/features/team/team-page/team-page.ts
@@ -1,9 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { UserService } from '../../../core/services/user/user-service';
+import { UserCard } from "../user-card/user-card";
 
 @Component({
   selector: 'app-team-page',
-  imports: [],
+  imports: [UserCard],
   templateUrl: './team-page.html',
   styleUrl: './team-page.scss',
 })
-export class TeamPage {}
+export class TeamPage {
+  private readonly userService = inject(UserService);
+  users = this.userService.users;
+}
