50e9c05f8c0fb0b70936bb4bc6f9c0079f158c15
Abdelmomen Nasreldin <abdelmomennasreldin@gmail.com>
2026-03-04 15:16:59 +0200

    Auto-generated commit

---
+++ chunk-4YH6JESN.js
@@ -0,0 +1,1 @@
+import{Pb as i,_a as t,sb as n,tb as s}from"./chunk-7UQOBNHL.js";var c=class a{static \u0275fac=function(e){return new(e||a)};static \u0275cmp=t({type:a,selectors:[["app-analytics-page"]],decls:2,vars:0,template:function(e,l){e&1&&(n(0,"p"),i(1,"analytics-page works!"),s())},encapsulation:2})};export{c as AnalyticsPage};
